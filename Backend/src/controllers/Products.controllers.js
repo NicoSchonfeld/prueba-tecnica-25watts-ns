4308e15513cdb56684ca6e44415acf1355904d25
Nico Schönfeld <75105918+NicoSchonfeld@users.noreply.github.com>
2023-10-12 13:58:49 -0300

    V4 - Se obtienen de manera random los objetos del array products.

--- Backend/src/controllers/Products.controllers.js
+++ Backend/src/controllers/Products.controllers.js
@@ -1,13 +1,13 @@
 const products = [
   {
-    title: "Aliquam at dui lorem ipsum",
+    title: "Aliquam post loralo lorem ipsum 1",
     item1: "- Ut mattis gravida efficitur",
     item2: "- Ut feugiat vehicula nibh, efficitur laoreet justo.",
     item3: "- Molestie id. Integer varius enim sit amet egestas.",
     item4: "- Ut mattis gravida efficitur.",
   },
   {
-    title: "Aliquam at dui hasu kduid",
+    title: "Aliquam at dui hasu kduid 2",
     item1: "- Ut mattis gravida efficitur",
     item2: "- Ut feugiat vehicula nibh, efficitur laoreet justo.",
     item3: "- Molestie id. Integer varius enim sit amet egestas.",
@@ -15,6 +15,14 @@ const products = [
   },
   {
     title: "Aliquam at dui lorem ipsum",
+    item1: "- Ut mattis gravida efficitur",
+    item2: "- Ut feugiat vehicula nibh, efficitur laoreet justo.",
+    item3: "- Molestie id. Integer varius enim sit amet egestas.",
+    item4: "- Ut mattis gravida efficitur.",
+  },
+
+  {
+    title: "Aliquam at dui Noea ipsum",
     item1: "- Ut mattis gravida efficitur",
     item2: "- Ut feugiat vehicula nibh, efficitur laoreet justo.",
     item3: "- Molestie id. Integer varius enim sit amet egestas.",
@@ -36,7 +44,6 @@ const products = [
     item3: "- Molestie id. Integer varius enim sit amet egestas.",
     item4: "- Ut mattis gravida efficitur.",
   },
-
   {
     title: "Aliquam at dui lorem ipsum",
     item1: "- Ut mattis gravida efficitur",
@@ -45,14 +52,7 @@ const products = [
     item4: "- Ut mattis gravida efficitur.",
   },
   {
-    title: "Aliquam at dui lorem ipsum",
-    item1: "- Ut mattis gravida efficitur",
-    item2: "- Ut feugiat vehicula nibh, efficitur laoreet justo.",
-    item3: "- Molestie id. Integer varius enim sit amet egestas.",
-    item4: "- Ut mattis gravida efficitur.",
-  },
-  {
-    title: "Aliquam at dui lorem ipsum",
+    title: "Tulopo at dui lorem ipsum",
     item1: "- Ut mattis gravida efficitur",
     item2: "- Ut feugiat vehicula nibh, efficitur laoreet justo.",
     item3: "- Molestie id. Integer varius enim sit amet egestas.",
@@ -109,4 +109,23 @@ const products = [
   },
 ];
 
-export const getProducts = (req, res) => res.json(products);
+const randomProducts = (products) => {
+  // Recibe como parametro un array
+  // i debe ser 1 menos a la longitud del array ya que se genera un numero aleatorio que este dentro del rango de la longitud.
+
+  for (let i = products?.length - 1; i > 0; i--) {
+    const randomNum = Math.floor(Math.random() * (i + 1));
+
+    // almacenamos el valor que se encuentre en la posicion products[i]
+    const posicionArray = products[i];
+
+    // Realizamos los cambios
+    products[i] = products[randomNum];
+    products[randomNum] = posicionArray;
+  }
+
+  return products;
+};
+
+export const getProducts = (req, res) =>
+  res.status(200).json(randomProducts(products));
